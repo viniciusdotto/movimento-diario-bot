aeafad2be321f345ee8889c4d32ed5b264a4ce5f
viniciusdotto <dotto.vinicius7@gmail.com>
2024-05-25 20:56:21 -0300

    Cria primeira versão do bot

---
+++ bot.js
@@ -0,0 +1,61 @@
+import express from 'express';
+import TelegramBot from 'node-telegram-bot-api';
+import 'dotenv/config'
+
+const token = process.env.API_TOKEN;
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+  const username = msg.from.username || 'atleta';
+  const welcomeMessage = `Olá ${username}, bem-vindo ao Movimento Diário, seu gerador de treinos e periodizações! Como posso te ajudar?`;
+
+  if (msg.text === '/quit') {
+    bot.sendMessage(chatId, 'Volte sempre, se precisar de algo só mandar uma mensagem');
+    return;
+  }
+
+  const options = {
+    reply_markup: {
+      inline_keyboard: [
+        [{ text: 'Treino de Hoje', callback_data: 'currentWorkout' }],
+        [{ text: 'Gerar Novo Treino', callback_data: 'newWorkout' }]
+      ]
+    }
+  };
+
+  bot.sendMessage(chatId, welcomeMessage, options);
+});
+
+bot.on('callback_query', (callbackQuery) => {
+  const msg = callbackQuery.message;
+  const chatId = msg.chat.id;
+  const data = callbackQuery.data;
+
+  let response;
+
+  switch (data) {
+    case 'currentWorkout':
+      response = 'O seu treino de hoje é: ...';
+      break;
+    case 'newWorkout':
+      response = 'Para gerar o novo treino, preciso que você responda algumas perguntas...';
+      break;
+    default:
+      response = 'Por favor, escolha uma opção válida.';
+  }
+
+  bot.sendMessage(chatId, response);
+  bot.answerCallbackQuery(callbackQuery.id);
+});
+
+bot.on('polling_error', (error) => {
+  console.error(`[polling_error] ${error.code}: ${error.message}`);
+});
+
+const app = express();
+app.use(express.json());
+
+app.listen(3000, () => {
+  console.log("API Started!");
+});
