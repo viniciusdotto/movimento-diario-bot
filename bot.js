72ce3f1c0fe18541f9dea276bfc5cc2cdd71fb25
viniciusdotto <dotto.vinicius7@gmail.com>
2024-06-09 18:37:47 -0300

    refactor: Isola o banco de dados do arquivo principal

--- bot.js
+++ bot.js
@@ -2,27 +2,11 @@ import express from 'express';
 import TelegramBot from 'node-telegram-bot-api';
 import 'dotenv/config'
 
-import sequelize from './sequelize.js';
-import User from './models/user.js';
-import Equipment from './models/equipment.js';
-import Exercise from './models/exercise.js';
-import Workout from './models/workout.js';
-import Repetition from './models/repetition.js';
+import Database from './db.js';
 import './models/userEquipment.js';
 
-(async () => {
-  try {
-    await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
-
-    // Sync all models
-    await sequelize.sync({ force: true });  // Use { force: true } to drop and recreate the tables for development
-    console.log('All models were synchronized successfully.');
-
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
-})();
+const db = new Database()
+db.init();
 
 const token = process.env.API_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
---
+++ db.js
@@ -0,0 +1,25 @@
+import User from './models/user.js';
+import Equipment from './models/equipment.js';
+import Exercise from './models/exercise.js';
+import Workout from './models/workout.js';
+import Repetition from './models/repetition.js';
+
+import sequelize from './sequelize.js';
+
+class Database {
+  async init() {
+    try {
+      await sequelize.authenticate();
+      console.log('Connection has been established successfully.');
+  
+      // Sync all models
+      await sequelize.sync({ force: true });  // Use { force: true } to drop and recreate the tables for development
+      console.log('All models were synchronized successfully.');
+  
+    } catch (error) {
+      console.error('Unable to connect to the database:', error);
+    }
+  }
+}
+
+export default Database;
